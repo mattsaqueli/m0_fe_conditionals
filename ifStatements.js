043c21dffeb408e325ba459a4da5b69d0199fa4b
Matt Saqueli <msaqueli22@turing.edu>
2023-01-11 13:37:43 -0600

    Add conditional statements to part 2

--- ifStatements.js
+++ ifStatements.js
@@ -42,10 +42,12 @@ if (weather === "sunny") {
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-var numQuarters = 0
+var numQuarters = 2
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+if (numQuarters >= 2) {
+  console.log("I have enough money for a gumball");
+} else (numQuarters < 2) 
+  console.log("I don't have enough money for a gumball");
 
 
 // #####################
